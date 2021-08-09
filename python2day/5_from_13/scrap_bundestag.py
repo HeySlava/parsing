d2d941a911784c0585367744f328c61ac54f3f36
Slava K <vyach.kapitonov@gmail.com>
2021-08-09 18:51:39 +0300

    got links of all candidates

--- python2day/5_from_13/scrap_bundestag.py
+++ python2day/5_from_13/scrap_bundestag.py
@@ -1,8 +1,22 @@
-import requests
 from bs4 import BeautifulSoup
+import requests
+import time
 
-
+persons_link = []
 for i in range(0, 740, 20):
     url = f"https://www.bundestag.de/ajax/filterlist/en/members/453158-453158/h_a45203fd0f1592191f1bda63b5d86d72?limit=20&noFilterSet=true&offset={i}"
-    print(url)
+
+    q = requests.get(url)
+    result = q.content
+
+    soup = BeautifulSoup(result, 'lxml')
+    persons = soup.select('.bt-slide-content a')
+
+    for person in persons:
+        persons_link.append(person.get('href'))
+    time.sleep(1)
+
+with open('candidate_links.txt', 'a') as f:
+    for person_link in persons_link:
+        f.write(f'{person}\n')
 
