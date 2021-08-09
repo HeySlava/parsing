4bec01a4c5e99b18bc2df600ccbf45a4f898f70a
Slava K <vyach.kapitonov@gmail.com>
2021-08-10 00:03:02 +0300

    saved separetally html page of each condidate

--- python2day/5_from_13/scrap_bundestag.py
+++ python2day/5_from_13/scrap_bundestag.py
@@ -2,22 +2,39 @@ from bs4 import BeautifulSoup
 import requests
 import time
 
-persons_link = []
-for i in range(0, 740, 20):
-    url = f"https://www.bundestag.de/ajax/filterlist/en/members/453158-453158/h_a45203fd0f1592191f1bda63b5d86d72?limit=20&noFilterSet=true&offset={i}"
+# persons_link = []
+# for i in range(0, 740, 20):
+#     url = f"https://www.bundestag.de/ajax/filterlist/en/members/453158-453158/h_a45203fd0f1592191f1bda63b5d86d72?limit=20&noFilterSet=true&offset={i}"
 
+#     q = requests.get(url)
+#     result = q.content
+
+#     soup = BeautifulSoup(result, 'lxml')
+#     persons = soup.select('.bt-slide-content a')
+
+#     for person in persons:
+#         persons_link.append(person.get('href'))
+#     time.sleep(1)
+#     print(i)
+
+# with open('candidate_links.txt', 'a') as f:
+#     for person in persons_link:
+#         f.write(f'{person}\n')
+
+with open('candidate_links.txt') as f:
+    links = [link.strip() for link in f.readlines()]
+
+count = 0
+for url in links:
     q = requests.get(url)
-    result = q.content
+    # soup = BeautifulSoup(q.content, 'lxml')
 
-    soup = BeautifulSoup(result, 'lxml')
-    persons = soup.select('.bt-slide-content a')
+    # name = soup.select_one('.bt-biografie-beruf p').text
 
-    for person in persons:
-        persons_link.append(person.get('href'))
-    time.sleep(1)
-    print(i)
+    count += 1
+    with open(f'data/{count}', 'w') as f:
+        f.write(q.text)
 
-with open('candidate_links.txt', 'a') as f:
-    for person in persons_link:
-        f.write(f'{person}\n')
+    print(f"Done {count} from {len(links)}")
+    time.sleep(0.5)
 
