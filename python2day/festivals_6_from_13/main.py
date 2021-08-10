afcfeb7a5649acc8b54ffe84d53f4749cd3bbc1d
Slava K <vyach.kapitonov@gmail.com>
2021-08-10 13:59:32 +0300

    saved first page

--- python2day/festivals_6_from_13/main.py
+++ python2day/festivals_6_from_13/main.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import json
+import json
 import requests
 import time
 
@@ -8,7 +9,14 @@ headers = {
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"
         }
 
-for i in range(0, 24, 168):
-    print(i)
+for i in range(0, 192, 24):
+    url = f'https://www.skiddle.com/festivals/search/?ajaxing=1&sort=0&fest_name=&from_date=10%20Aug%202021&to_date=&maxprice=500&o={i}&bannertitle=August'
+    req = requests.get(url=url, headers=headers)
+    json_data = json.loads(req.text)
+    html_response = json_data['html']
 
-    # url = f'https://www.skiddle.com/festivals/search/?ajaxing=1&sort=0&fest_name=&from_date=10%20Aug%202021&to_date=&maxprice=500&o={i}&bannertitle=August'
+    with open(f'data/index_{i}.html', 'w') as f:
+        f.write(html_response)
+
+    break
+
