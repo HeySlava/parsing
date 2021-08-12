a7bef649f3114e3fa21a173dc89e00d7a51c03ee
Slava K <vyach.kapitonov@gmail.com>
2021-08-12 17:59:59 +0300

    downloaded all pages with watch

--- python2day/watches_9_from_13/main.py
+++ python2day/watches_9_from_13/main.py
@@ -5,18 +5,25 @@ import requests
 
 
 def get_all_pages():
-    headers = {
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"}
+    # headers = {
+    #         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"}
 
-    r = requests.get(url="https://shop.casio.ru/catalog/g-shock/",
-                     headers=headers)
+    # r = requests.get(url="https://shop.casio.ru/catalog/g-shock/",
+    #                  headers=headers)
 
-    if not os.path.exists('data'):
-        print('This folder is not exists')
-        os.mkdir('data')
+    # if not os.path.exists('data'):
+    #     print('This folder is not exists')
+    #     os.mkdir('data')
 
-    with open('data/page_1.html', 'w') as file:
-        file.write(r.text)
+    # with open('data/page_1.html', 'w') as file:
+    #     file.write(r.text)
+
+    with open('data/page_1.html') as file:
+        src = file.read()
+
+    soup = BeautifulSoup(src, 'lxml')
+    page_count = soup.select_one('.bx-pagination-container li').text
+    print(page_count)
 
 
 def main():
