021b9d6b39a5fad9151acfafd92354630b366d02
Slava K <vyach.kapitonov@gmail.com>
2021-08-13 16:42:21 +0300

    get atricle, price, href from each watch

--- python2day/watches_9_from_13/main.py
+++ python2day/watches_9_from_13/main.py
@@ -2,32 +2,77 @@ from bs4 import BeautifulSoup
 import lxml
 import os
 import requests
+import time
 
 
 def get_all_pages():
-    # headers = {
-    #         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"}
+
+    headers = {
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"}
 
     # r = requests.get(url="https://shop.casio.ru/catalog/g-shock/",
     #                  headers=headers)
 
-    # if not os.path.exists('data'):
-    #     print('This folder is not exists')
-    #     os.mkdir('data')
+    if not os.path.exists('data'):
+        print('This folder is not exists')
+        os.mkdir('data')
 
     # with open('data/page_1.html', 'w') as file:
     #     file.write(r.text)
 
-    with open('data/page_1.html') as file:
-        src = file.read()
+#     with open('data/page_1.html') as file:
+#         src = file.read()
 
-    soup = BeautifulSoup(src, 'lxml')
-    page_count = soup.select_one('.bx-pagination-container li').text
-    print(page_count)
+#     soup = BeautifulSoup(src, 'lxml')
+#     page_count = int(soup.select('.bx-pagination-container li a')[-2].text)
+
+#     for page in range(1, page_count + 1):
+#         url = f'https://shop.casio.ru/catalog/g-shock/?PAGEN_1={page}'
+
+#         r = requests.get(url=url, headers=headers)
+
+#         with open (f'data/page_{page}.html', 'w') as file:
+#             file.write(r.text)
+
+#         time.sleep(1)
+#         print(f'Done {page}/{page_count}')
+
+#     return page_count + 1
+
+
+def collect_data():
+
+    pages = os.listdir('data')
+
+    watches = []
+    data = []
+    for page in pages:
+        with open(f'data/{page}') as file:
+            src = file.read()
+        soup = BeautifulSoup(src, 'lxml')
+
+        watches.extend(soup.select('.product-item'))
+
+    for item in watches:
+
+        article = item.select_one('.product-item__articul').text.strip()
+        price = item.select_one('.product-item__price').text.strip()\
+                .lstrip("руб.").replace(' ', '')
+        href = 'https://shop.casio.ru/' + \
+                item.select_one('a')['href'].strip()
+
+        data.append(
+                {
+                    "article": article,
+                    "price": price,
+                    "href": href
+                }
+            )
 
 
 def main():
-    get_all_pages()
+    # pages_count = get_all_pages()
+    collect_data()
 
 
 if __name__ == '__main__':
