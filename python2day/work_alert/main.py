68144ace335dfac3b824ffaa0987bf3f85b46891
Slava desktop <vyach.kapitonov@gmail.com>
2021-09-04 04:17:51 +0300

    got all info from each post

--- python2day/work_alert/main.py
+++ python2day/work_alert/main.py
@@ -1,18 +1,22 @@
-from datetime import datetime
-from selenium import webdriver
+import json
 import os
 import pickle
 import time
-import json
+from datetime import datetime
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
 
 
 def get_data():
+
+    if not  os.path.exists('data'):
+        os.mkdir('data')
     
     try:
 
         options = webdriver.ChromeOptions()
-        options.add_argument("--no-sandbox")
-        options.add_argument("headless")
+        # options.add_argument("--no-sandbox")
+        # options.add_argument("headless")
         options.add_argument("--disable-blink-features=AutomationControlled")
 
         driver = webdriver.Chrome(
@@ -20,17 +24,47 @@ def get_data():
                 options=options
             )
 
-        link = 'https://www.fl.ru/'
+        link = 'https://www.fl.ru/projects/'
         
-        # driver.get(link)
-        # time.sleep(1)
-        # test = driver.select('h2.b-page__title')
-        # print(test)
+        driver.get(link)
+        time.sleep(1)
+
+        driver.find_element_by_css_selector('#comboe').send_keys('Программирование')
+        time.sleep(0.5)
+        driver.find_element_by_css_selector('#comboe').send_keys('парсинг данных')
+        time.sleep(1)
+        driver.find_element_by_css_selector('.b-buttons .b-button').click()
+
+        data = []
+        projects = driver.find_elements_by_css_selector('.b-post__grid')
+        for project in projects:
+            project_id = project.find_element_by_css_selector('a').get_attribute('name')
+            project_title = project.find_element_by_css_selector('a').text
+            project_href = "https://www.fl.ru" + \
+                    project.find_element_by_css_selector('a').get_attribute('href')
+            project_price = project.find_element_by_css_selector('.b-post__price').text
+            project_descr = project.find_element_by_css_selector('.b-post__txt').text
+
+            data.append(
+                    {
+                        'id': project_id,
+                        'items': {
+                            'title': project_title,
+                            'href': project_href,
+                            'description': project_descr,
+                            'price': project_price
+                        }
+                    }
+                )
+            print(data)
+            break
+
 
         html = driver.page_source
 
-        with open('index.html', 'w') as file:
-            json.dump(html, file, indent=4, ensure_ascii=False)
+        html = driver.page_source
+        with open('data/html.html', 'w') as file:
+            file.write(html)
 
     finally:
         driver.quit()
