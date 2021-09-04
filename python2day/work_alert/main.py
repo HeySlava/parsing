c6b68d0dea3627c8e20854ee2225cf41eac9edbf
Slava desktop <vyach.kapitonov@gmail.com>
2021-09-04 05:26:49 +0300

    found error and print id from json

--- python2day/work_alert/main.py
+++ python2day/work_alert/main.py
@@ -7,63 +7,6 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 
 
-def get_data():
-
-    if not  os.path.exists('data'):
-        os.mkdir('data')
-    
-    try:
-
-        options = webdriver.ChromeOptions()
-        # options.add_argument("--no-sandbox")
-        options.add_argument("headless")
-        options.add_argument("--disable-blink-features=AutomationControlled")
-
-        driver = webdriver.Chrome(
-                executable_path=r"/usr/bin/chromedriver",
-                options=options
-            )
-
-        link = 'https://www.fl.ru/projects/'
-        
-        driver.get(link)
-        time.sleep(1)
-
-        driver.find_element_by_css_selector('#comboe').send_keys('Программирование')
-        time.sleep(0.5)
-        driver.find_element_by_css_selector('#comboe').send_keys('парсинг данных')
-        time.sleep(1)
-        driver.find_element_by_css_selector('.b-buttons .b-button').click()
-
-        current_time = int(time.time())
-        data = []
-        projects = driver.find_elements_by_css_selector('.b-post__grid')
-        for project in projects:
-            project_id = project.find_element_by_css_selector('a').get_attribute('name')
-            project_title = project.find_element_by_css_selector('a').text
-            project_href = "https://www.fl.ru" + \
-                    project.find_element_by_css_selector('a').get_attribute('href')
-            project_price = project.find_element_by_css_selector('.b-post__price').text
-            project_descr = project.find_element_by_css_selector('.b-post__txt').text
-
-            data.append(
-                    {
-                        'id': project_id,
-                        'items': {
-                            'title': project_title,
-                            'href': project_href,
-                            'description': project_descr,
-                            'price': project_price,
-                            'time': f'{datetime.datetime.now().strftime("%d/%m/%Y %H:%M")}'
-                            
-                        }
-                    }
-                )
-
-        with open('data/posts.json', 'w', encoding='utf-8') as file:
-            json.dump(data, file, indent=4, ensure_ascii=False)
-
-
 def get_list():
 
     try:
@@ -90,47 +33,48 @@ def get_list():
         driver.find_element_by_css_selector('.b-buttons .b-button').click()
         time.sleep(1)
         projects = driver.find_elements_by_css_selector('.b-post__grid')
-        projects_title = [project.find_element_by_css_selector('a').text for project in projects]
-        return zip(projects_title, projects)
+        projects_id = [project.find_element_by_css_selector('a').get_attribute('id') for project in projects]
+        return zip((projects_id, projects))
+
     finally:
         driver.quit()
 
 
-
 def save_data(project):
 
+    try:
 
-def save_data(project):
-        current_time = int(time.time())
-        data = []
-        for project in projects:
-            project_id = project.find_element_by_css_selector('a').get_attribute('name')
-            project_title = project.find_element_by_css_selector('a').text
-            project_href = "https://www.fl.ru" + \
-                    project.find_element_by_css_selector('a').get_attribute('href')
-            project_price = project.find_element_by_css_selector('.b-post__price').text
-            project_descr = project.find_element_by_css_selector('.b-post__txt').text
+        project_id = project.find_element_by_css_selector('a').get_attribute('name')
+        project_title = project.find_element_by_css_selector('a').text
+        project_href = "https://www.fl.ru" + \
+                project.find_element_by_css_selector('a').get_attribute('href')
+        project_price = project.find_element_by_css_selector('.b-post__price').text
+        project_descr = project.find_element_by_css_selector('.b-post__txt').text
 
-            data.append(
-                    {
-                        'id': project_id,
-                        'items': {
-                            'title': project_title,
-                            'href': project_href,
-                            'description': project_descr,
-                            'price': project_price,
-                            'time': f'{datetime.datetime.now().strftime("%d/%m/%Y %H:%M")}'
-                            
-                        }
+        data = {
+                'id': project_id,
+                'items': {
+                    'title': project_title,
+                    'href': project_href,
+                    'description': project_descr,
+                    'price': project_price,
+                    'time': f'{datetime.datetime.now().strftime("%d/%m/%Y %H:%M")}'
+
                     }
-                )
+            }
+    finally:
 
-        with open('data/posts.json', 'w', encoding='utf-8') as file:
+        with open('data/posts.json', 'a', encoding='utf-8') as file:
             json.dump(data, file, indent=4, ensure_ascii=False)
 
-    finally:
-        driver.quit()
+def get_update():
+
+    with open('data/posts.json', encoding='utf-8') as file:
+        projects = json.load(file)
+
+    for i in projects:
+        print(i['id'])
 
 
 if __name__ == '__main__':
-    get_data()
+    get_update()
